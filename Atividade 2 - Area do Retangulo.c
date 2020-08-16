1cd3143c95850172d411fbecd7dbdf9137b2fb18
Diego Jesus <diegojs03@hotmail.com>
2020-08-16 09:51:23 -0300

    Area do retangulo

---
+++ Atividade 2 - Area do Retangulo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	int a, b, area;
+	
+	printf("Digite a base do retangulo\n");
+	scanf("%d", &a);
+	printf("Digite a altura do retangulo\n");
+	scanf("%d", &b);
+	area = a*b;
+	printf("A area do retangulo e: %d", area);
+}
+
+	
+	
+	
