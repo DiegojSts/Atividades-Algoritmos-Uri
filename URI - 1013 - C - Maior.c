6e4aa5bd05b34d96782eaab073ce58ad047abcff
Diego Jesus <diegojs03@hotmail.com>
2020-08-18 07:41:27 -0300

    URI - Resoluções em C - Diversos

---
+++ URI - 1013 - C - Maior.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+/*
+Faça um programa que leia três valores e apresente o maior dos três valores lidos seguido da mensagem “eh o maior”. 
+Utilize a fórmula:
+
+Obs.: a fórmula apenas calcula o maior entre os dois primeiros (a e b). Um segundo passo, portanto é necessário para chegar no resultado esperado.
+
+Entrada
+O arquivo de entrada contém três valores inteiros.
+
+Saída
+Imprima o maior dos três valores seguido por um espaço e a mensagem "eh o maior".
+
+*/
+ 
+int main() {
+ 
+    int a, b, c, MaiorAB, MaiorABC;
+    
+    scanf("%d %d %d", &a, &b, &c);
+    
+    MaiorAB = (a + b + abs(a-b)) /2;
+    
+    MaiorABC = (MaiorAB + c + abs(MaiorAB-c))/2;
+    
+    printf("%d eh o maior\n", MaiorABC);
+ 
+    return 0;
+}
